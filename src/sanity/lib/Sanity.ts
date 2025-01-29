202bbaaf7eb7e54da37984d24deb8584472ea1e0
Bilawal <161321582+Bilawalakber93@users.noreply.github.com>
2025-01-29 23:33:40 +0500

    Update Sanity.ts

--- src/sanity/lib/Sanity.ts
+++ src/sanity/lib/Sanity.ts
@@ -3,9 +3,9 @@ import { createClient } from "@sanity/client";
 
 // Ensure backend environment variables are being read
 const sanity = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'default_project_id',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2021-08-31',
+  projectId: process.env.SANITY_PROJECT_ID || 'default_project_id',
+  dataset: process.env.SANITY_DATASET || 'production',
+  apiVersion: process.env.SANITY_API_VERSION || '2021-08-31',
   token: process.env.SANITY_API_TOKEN,
   useCdn: false,
 });
